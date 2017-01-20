f4d20bda7c59c51374b7c90f4646d1863b67170e
Molly Lewis <mll@stanford.edu>
2017-01-20 14:17:45 -0600

    add exp3 and 4

--- experiments/exp3/js/exp3.js
+++ experiments/exp3/js/exp3.js
@@ -1,4 +1,4 @@
-// XTMEM 2 -- Replication of SPSS Exp2
+// XTMEM 3 -- Replication of XT 2007a with SPSS1 method (3sub first and diff labels for sub and one)
 // Overview: 
 //      (1) Helper
 //      (2) Parameters and Stimulus Setup 
@@ -10,6 +10,7 @@
 /*Shows slides. We're using jQuery here the $ is the jQuery selector function, 
 which takes as input either a DOM element or a CSS selector string. */
 function showSlide(id) {
+
   $(".slide").hide(); // Hide all slides
   $("#"+id).show(); //Show just the slide we want to show
 }
@@ -40,38 +41,37 @@ $.fn.preload = function() {
 };
 
 
-
-
 // ---------------- 2. PARAMETER SETUP ------------------
 var total_trials = 12;
 
 // -- Words--
-var words = shuffle(["wug", "rab", "fep", "rif", "mep", "tob",  "tev", "sib", "zom"])
-var sub_words = words.slice(0,3)
-var other_words = words.slice(3,9)
+var words = shuffle(["wug", "rab", "fep", "rif", "mep", "tob",  "tev", "sib", "zom", "nur", "dit", "pag"  ])
 
 // -- Pics -
 var test_pics = ["c1_sub1", "c1_sub2", "c1_bas1", "c1_bas2", "c1_sup1", "c1_sup2", "c1_sup3", "c1_sup4",
                  "c2_sub1", "c2_sub2", "c2_bas1", "c2_bas2", "c2_sup1", "c2_sup2", "c2_sup3", "c2_sup4",
                  "c3_sub1", "c3_sub2", "c3_bas1", "c3_bas2", "c3_sup1", "c3_sup2", "c3_sup3", "c3_sup4"]
 
-var train_one_pics = shuffle(["c1_sub3","c2_sub3","c3_sub3"])
-var train_three_pics = shuffle([["c1_sub3", "c1_sub4", "c1_sub5"], 
-                                ["c2_sub3", "c2_sub4", "c2_sub5"], 
-                                ["c3_sub3", "c3_sub4", "c3_sub5"],
-                                ["c1_sub3", "c1_bas3", "c1_bas4"], 
-                                ["c2_sub3", "c2_bas3", "c2_bas4"],  
-                                ["c3_sub3", "c3_bas3", "c3_bas4"],
-                                ["c1_sub3", "c1_sup5", "c1_sup6"], 
-                                ["c2_sub3", "c2_sup5", "c2_sup6"],  
-                                ["c3_sub3", "c3_sup5", "c3_sup6"]])
+var train_one_pics = shuffle([["c1_sub3"],["c2_sub3"],["c3_sub3"]])
 
-// shuffle block order with constraint that one or sub appears first
-var  blocks = ["one", "three"]
+var train_3sub_pics = shuffle([["c1_sub3", "c1_sub4", "c1_sub5"], 
+                               ["c2_sub3", "c2_sub4", "c2_sub5"], 
+                               ["c3_sub3", "c3_sub4", "c3_sub5"]])
 
+var train_3bas_pics = shuffle([["c1_sub3", "c1_bas3", "c1_bas4"], 
+                               ["c2_sub3", "c2_bas3", "c2_bas4"],  
+                               ["c3_sub3", "c3_bas3", "c3_bas4"]])
+
+var train_3sup_pics = shuffle([["c1_sub3", "c1_sup5", "c1_sup6"], 
+                               ["c2_sub3", "c2_sup5", "c2_sup6"],  
+                               ["c3_sub3", "c3_sup5", "c3_sup6"]])
+
+// shuffle block order with constraint that sub appears first
+second_blocks = shuffle(["one", "3bas", "3sup"])
+var  blocks = ["3sub"].concat(second_blocks);
 
 // PRE-LOAD IMAGES
-var all_pics = [].concat.apply([], [train_one_pics, train_three_pics, test_pics], "mrfrog.png")
+var all_pics = [].concat.apply([], [train_one_pics, train_3sub_pics, train_3bas_pics, train_3sup_pics], "mrfrog.png")
 all_pics = [].concat.apply([], all_pics)
 
 var images = []
@@ -88,8 +88,6 @@ var current_trial = 1
 var current_trial_in_block = 0
 var current_train_pics = []
 var current_word
-var t
-var t2
 
 // START experiment
 showSlide("instructions");
@@ -108,27 +106,14 @@ var experiment = {
     /*test*/
     test: function() {  
 
-        // pic logic
+      // pic logic
         current_train_pics = shuffle(eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]"))
-         
-       if (blocks[0] == "three") {
         var objs = eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]").join()
-        var sub_trial = objs.includes("sub5") // picks out sub trials for matching word for sub and one trials
-       }
 
         // word logic
-        if (blocks[0] == "one") {
-          //var cat = train_one_pics[current_trial_in_block]
-          current_word = sub_words[current_trial_in_block]
-          current_train_pics[0] = current_train_pics
-        } else if (sub_trial){
-          var cat_type = objs.split("_")[0] 
-          var cat = cat_type + "_sub3"
-          current_word = sub_words[train_one_pics.indexOf(cat)]
-        } else {
-          current_word = other_words[0]
-          other_words.shift()
-        }
+        current_word = words[0]
+        words.shift()
+  
 
         // build question html
         if (blocks[0] == "one") {
@@ -145,13 +130,12 @@ var experiment = {
         train_pics_html = '<table align="center"><tr>'
         if (blocks[0] == "one") {
             train_pics_html += '<td align="center">'
-            train_pics_html += '<img class = "tohide" style="visibility: hidden;"  src ="images/' + current_train_pics + '.jpg"' 
+            train_pics_html += '<img src ="images/' + current_train_pics + '.jpg"' 
             train_pics_html += 'alt="Stanford University" width="110"></td>'
         } else {
-           for (i=0;i<current_train_pics.length;i++){
+          for (i=0;i<current_train_pics.length;i++){
             train_pics_html += '<td align="center">'
-            train_pics_html += '<img class = "tohide" src ="images/' + current_train_pics[i] + '.jpg"' 
-            train_pics_html += '<img style="visibility: hidden;" id="pic' + i + '"src ="images/' + current_train_pics[i] + '.jpg"' 
+            train_pics_html += '<img src ="images/' + current_train_pics[i] + '.jpg"' 
             train_pics_html += 'alt="Stanford University" width="110"></td>'
           }
         }
@@ -161,7 +145,7 @@ var experiment = {
 
         // build test table of images
         test_pics = shuffle(test_pics)
-        test_pics_html = '<table align="center" >'
+        test_pics_html = '<table align="center">'
         test_pics_html += '<tr><td colspan="5"><p style="font-size:16px;text-align:left; font-style:italic" > To give a ' + current_word + ', click on it below. When you have given all the ' + current_word + 's, click the Next button. </p></td></tr><tr>'
         for (i=0;i<=24-1;i++){
           test_pics_html += '<td align="center" > <input src="images/' + test_pics[i] + '.jpg" type="image"'
@@ -171,82 +155,15 @@ var experiment = {
             test_pics_html += "</tr><tr>"
           }
         }
-        test_pics_html += '</tr></table></div>'
+        test_pics_html += '</tr></table>'
+        $("#testPics").html(test_pics_html)
 
-        // DISPLAY HTML
         $("#question1").html(questionprompt_html1) 
+        $("#question2").html(questionprompt_html2)  
+        $("#counter").html(current_trial + ' / ' + total_trials) 
+  
 
-        // this deals with looping of the training images and showing question2, test images, button and counter
-        if (blocks[0] != "one") {
-
-          var images = document.getElementsByClassName("tohide");
-          document.getElementById("testtable").style.display = "none";
-
-          // FROM: http://stackoverflow.com/questions/18351395/hide-and-display-images-for-a-time-in-a-special-time-period
-          (function(){
-          var loop = 0
-          var i = 2
-
-           t = setInterval(function(){
-
-            images[i].style.visibility = "hidden";
-            if (i+1< images.length) {
-              i++;
-            }
-            else {
-              i = 0;
-              loop++
-            } 
-             images[i].style.visibility = "visible";
-             
-             // show test images on third loop (6000 seconds)
-             if (loop == 3 & i == 0) {
-                $("#question2").html(questionprompt_html2) 
-                $("#testPics").html(test_pics_html)
-                $("#counter").html(current_trial + ' / ' + total_trials) 
-                document.getElementById("testtable").style.display = "inline";
-             }
-
-            }, 1000) // time per pictures
-          })();
-
-      } else {
-
-          var images = document.getElementsByClassName("tohide");
-          document.getElementById("testtable").style.display = "none";
-
-          (function(){
-          var loop = 0
-          var i = 0
-
-           t = setInterval(function(){
-
-            if (images[i].style.visibility == "hidden") {
-              images[i].style.visibility = "visible";
-            } else {
-              images[i].style.visibility = "hidden";
-            }
-
-            loop++
-
-             // show test images on third loop (6000 seconds)
-             if (loop == 12) {
-                $("#question2").html(questionprompt_html2) 
-                $("#testPics").html(test_pics_html)
-                $("#counter").html(current_trial + ' / ' + total_trials) 
-                document.getElementById("testtable").style.display = "inline";
-             }
-
-            }, 1000) // time per pictures
-          })();
-
-        //$("#question2").html(questionprompt_html2) 
-        //$("#testPics").html(test_pics_html)
-        //$("#counter").html(current_trial + ' / ' + total_trials)  
-      }
-
-      showSlide('grid')
-
+        showSlide('grid')
     },
 
     highlight: function(el, given_obj) {
@@ -268,14 +185,9 @@ var experiment = {
 
     next: function() {
       if (selected.length > 0){
-          clearTimeout(t)
 
-          var current_cat
-          if (blocks[0] == "one") {
-              current_cat  = current_train_pics.split("_")[0]
-          } else {
-              current_cat  = current_train_pics[0].split("_")[0] 
-          }
+          var current_cat = current_train_pics[0].split("_")[0] 
+
           var current_cat_name
           if (current_cat == "c1"){
             current_cat_name = "vegetables"
@@ -285,33 +197,15 @@ var experiment = {
             current_cat_name = "animals"
           }
 
-          var current_cond
-          var current_train_pics_string
-          if (blocks[0] == "one"){
-            current_cond = "one"
-            current_train_pics_string = current_train_pics
-          } else {
-            current_train_pics_string = current_train_pics.join("-")
-            objs = eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]").join()
-            
-            if (objs.includes("sub5")) {
-                current_cond = "three_subordinate"
-            } else if (objs.includes("bas4")) {
-                current_cond = "three_basic"
-            } else if (objs.includes("sup6")) {
-                current_cond = "three_superordinate"
-            }
-          }
-      
           // save data
-          eval('experiment.condition_T' + current_trial + ' = ' + "'" + current_cond + "'" )
+          eval('experiment.condition_T' + current_trial + ' = ' + "'" + blocks[0] + "'" )
           eval('experiment.category_T' + current_trial + ' = ' + "'" + current_cat_name + "'" )
-          eval('experiment.trainBlock_T' + current_trial + ' = ' + "'" + blocks[0] + "'" )
+          eval('experiment.trainBlock_T' + current_trial + ' = ' + "'" + Math.floor((current_trial+2)/3) + "'" )
           eval('experiment.word_T' + current_trial + ' = ' + "'" + current_word + "'" )
-          eval('experiment.trainPics_T' + current_trial + ' = ' + "'" + current_train_pics_string + "'" )
+          eval('experiment.trainPics_T' + current_trial + ' = ' + "'" + current_train_pics.join("-") + "'" )
           eval('experiment.selected_T' + current_trial + ' = ' + "'" + selected + "'" )
          
-         if(current_trial == 3) {
+         if(current_trial%3 == 0) {
             blocks.shift()
             current_trial_in_block = 0
           } else {
--- experiments/exp4/js/exp4_old.js
+++ experiments/exp4/js/exp4_old.js
@@ -1,4 +1,4 @@
-// XTMEM 2 -- Replication of SPSS Exp2
+// XTMEM 4 -- Exact replication of SPSS Exp2, with same trial order and number of labels
 // Overview: 
 //      (1) Helper
 //      (2) Parameters and Stimulus Setup 
@@ -46,32 +46,37 @@ $.fn.preload = function() {
 var total_trials = 12;
 
 // -- Words--
-var words = shuffle(["wug", "rab", "fep", "rif", "mep", "tob",  "tev", "sib", "zom"])
+var words = shuffle(["wug", "rab", "fep", "rif", "mep", "tob",  "tev", "sib", "zom", "nam", "dax", "gid"])
 var sub_words = words.slice(0,3)
-var other_words = words.slice(3,9)
+var other_words = words.slice(3,12)
 
 // -- Pics -
 var test_pics = ["c1_sub1", "c1_sub2", "c1_bas1", "c1_bas2", "c1_sup1", "c1_sup2", "c1_sup3", "c1_sup4",
                  "c2_sub1", "c2_sub2", "c2_bas1", "c2_bas2", "c2_sup1", "c2_sup2", "c2_sup3", "c2_sup4",
                  "c3_sub1", "c3_sub2", "c3_bas1", "c3_bas2", "c3_sup1", "c3_sup2", "c3_sup3", "c3_sup4"]
 
-var train_one_pics = shuffle(["c1_sub3","c2_sub3","c3_sub3"])
-var train_three_pics = shuffle([["c1_sub3", "c1_sub4", "c1_sub5"], 
-                                ["c2_sub3", "c2_sub4", "c2_sub5"], 
-                                ["c3_sub3", "c3_sub4", "c3_sub5"],
-                                ["c1_sub3", "c1_bas3", "c1_bas4"], 
-                                ["c2_sub3", "c2_bas3", "c2_bas4"],  
-                                ["c3_sub3", "c3_bas3", "c3_bas4"],
-                                ["c1_sub3", "c1_sup5", "c1_sup6"], 
-                                ["c2_sub3", "c2_sup5", "c2_sup6"],  
-                                ["c3_sub3", "c3_sup5", "c3_sup6"]])
+var train_one_pics = shuffle([["c1_sub3"],["c2_sub3"],["c3_sub3"]])
+
+var train_3sub_pics = shuffle([["c1_sub3", "c1_sub4", "c1_sub5"], 
+                               ["c2_sub3", "c2_sub4", "c2_sub5"], 
+                               ["c3_sub3", "c3_sub4", "c3_sub5"]])
+
+var train_3bas_pics = shuffle([["c1_sub3", "c1_bas3", "c1_bas4"], 
+                               ["c2_sub3", "c2_bas3", "c2_bas4"],  
+                               ["c3_sub3", "c3_bas3", "c3_bas4"]])
+
+var train_3sup_pics = shuffle([["c1_sub3", "c1_sup5", "c1_sup6"], 
+                               ["c2_sub3", "c2_sup5", "c2_sup6"],  
+                               ["c3_sub3", "c3_sup5", "c3_sup6"]])
 
 // shuffle block order with constraint that one or sub appears first
-var  blocks = ["one", "three"]
+var  second_blocks = shuffle(["one",  "3bas", "3sup"])
 
+var blocks = [].concat.apply([], ["3sub", second_blocks])
 
 // PRE-LOAD IMAGES
-var all_pics = [].concat.apply([], [train_one_pics, train_three_pics, test_pics], "mrfrog.png")
+var all_pics = [].concat.apply([], [train_one_pics, train_3sub_pics, train_3bas_pics, 
+  train_3sup_pics, test_pics], "mrfrog.png")
 all_pics = [].concat.apply([], all_pics)
 
 var images = []
@@ -94,6 +99,7 @@ var t2
 // START experiment
 showSlide("instructions");
 
+
 // MAIN EXPERIMENT
 var experiment = {
 
@@ -108,27 +114,18 @@ var experiment = {
     /*test*/
     test: function() {  
 
+
         // pic logic
         current_train_pics = shuffle(eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]"))
          
-       if (blocks[0] == "three") {
-        var objs = eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]").join()
-        var sub_trial = objs.includes("sub5") // picks out sub trials for matching word for sub and one trials
-       }
+       //if (blocks[0] == "three") {
+        //var objs = eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]").join()
+        //var sub_trial = objs.includes("sub5") // picks out sub trials for matching word for sub and one trials
+       //}
 
         // word logic
-        if (blocks[0] == "one") {
-          //var cat = train_one_pics[current_trial_in_block]
-          current_word = sub_words[current_trial_in_block]
-          current_train_pics[0] = current_train_pics
-        } else if (sub_trial){
-          var cat_type = objs.split("_")[0] 
-          var cat = cat_type + "_sub3"
-          current_word = sub_words[train_one_pics.indexOf(cat)]
-        } else {
-          current_word = other_words[0]
-          other_words.shift()
-        }
+        current_word = other_words[0]
+        other_words.shift()
 
         // build question html
         if (blocks[0] == "one") {
@@ -271,11 +268,11 @@ var experiment = {
           clearTimeout(t)
 
           var current_cat
-          if (blocks[0] == "one") {
-              current_cat  = current_train_pics.split("_")[0]
-          } else {
+          //if (blocks[0] == "one") {
+          //   current_cat  = current_train_pics.split("_")[0]
+          //} else {
               current_cat  = current_train_pics[0].split("_")[0] 
-          }
+          //}
           var current_cat_name
           if (current_cat == "c1"){
             current_cat_name = "vegetables"
@@ -289,7 +286,7 @@ var experiment = {
           var current_train_pics_string
           if (blocks[0] == "one"){
             current_cond = "one"
-            current_train_pics_string = current_train_pics
+         //  current_train_pics_string = current_train_pics
           } else {
             current_train_pics_string = current_train_pics.join("-")
             objs = eval("train_" + blocks[0] + "_pics[" + current_trial_in_block + "]").join()
@@ -301,7 +298,8 @@ var experiment = {
             } else if (objs.includes("sup6")) {
                 current_cond = "three_superordinate"
             }
-          }
+         // }
+       }
       
           // save data
           eval('experiment.condition_T' + current_trial + ' = ' + "'" + current_cond + "'" )
