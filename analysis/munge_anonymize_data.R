6f93a1e608b5fa7073749ce78b722c7048acb173
Molly Lewis <mollyllewis@gmail.com>
2017-08-29 20:42:41 -0500

    reorganized, deployed SI

--- analysis/munge_anonymize_data.R
+++ analysis/munge_anonymize_data.R
@@ -1,4 +1,4 @@
-#### Munge and save anonymized version of XTMEM data for single exp###
+####g et one dataframe (all experiments) 
 
 
 # load libraries and pacakges
@@ -8,25 +8,27 @@ library(jsonlite)
 library(stringr)
 
 # set exp number
-EXPTNUM <- 12
+NUMEXPS <- 12
 
 ## Read in and process data
-raw_path <- paste0("../experiments/exp", EXPTNUM, "/production-results/")
-files <- dir(raw_path)
 d_raw <- data.frame()
-for (i in 1:length(files)[1]) {
-  s <- fromJSON(paste0(raw_path, files[i]))
-  s$answers$asses = ifelse(is.null(s$answers$asses), "NA", s$answers$asses)
-  d_raw <- bind_rows(d_raw, data.frame(s))
+for (j in 1:NUMEXPS){
+  files = dir(paste0("../experiments/exp", as.character(j), "/production-results/"))
+  for (i in 1:length(files)[1]) {
+    s <- fromJSON(paste0("../experiments/exp", as.character(j), "/production-results/", files[i]))
+    s$answers$asses = ifelse(is.null(s$answers$asses), "NA", s$answers$asses)
+    d_raw <- bind_rows(d_raw, data.frame(s, exp = j))
+  }
 }
+
+
 names(d_raw) <- str_replace(names(d_raw), "answers.", "")
 d_anonymized <- anonymize.sids(d_raw, "WorkerId")
 
-raw_anonymized_path <- paste0("../data/anonymized_raw_data/exp", EXPTNUM, "_A.csv")
-write_csv(d_anonymized, raw_anonymized_path)
+write_csv(d_anonymized, "../data/anonymized_data/all_raw_A.csv")
 
 # Munge
-d_anonymized <- read_csv(raw_anonymized_path)
+d_anonymized <- read_csv("../data/anonymized_data/all_raw_A.csv")
 
 d_anonymized_long = d_anonymized %>%
   gather(variable, value, contains("_")) %>%
@@ -39,15 +41,14 @@ d_anonymized_long = d_anonymized %>%
   mutate_if(is.character, funs(as.factor)) 
 
 d_anonymized_long_munged = d_anonymized_long %>%
-  select(subids, trial_num, category, condition, selected) %>%
+  select(exp, subids, trial_num, category, condition, selected) %>%
   mutate(selected = lapply(str_split(selected, ","), 
                            function(x) {str_sub(x, 4, 6)})) %>%
   mutate(prop_sub = unlist(lapply(selected, function(x){sum(x == "sub")/2})),
          prop_bas = unlist(lapply(selected, function(x){sum(x == "bas")/2})),
-         prop_sup = unlist(lapply(selected, function(x){sum(x == "sup")/4})),
-         exp = EXPTNUM) %>%
+         prop_sup = unlist(lapply(selected, function(x){sum(x == "sup")/4}))) %>%
   select(-selected)
 
 # Write
-write_csv(d_anonymized_long_munged, paste0("../data/munged_data/exp", EXPTNUM, "_data_munged.csv"))
+write_csv(d_anonymized_long_munged, paste0("../data/anonymized_data/all_data_munged_A.csv"))
 
